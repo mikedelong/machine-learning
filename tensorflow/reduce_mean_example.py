dffb5a693f9fc5d7418170f8e4e302e14684873f
Mike DeLong <mikedelong@outlook.com>
2017-01-17 09:46:12 -0500

    feature: the constant data is now part of the graph
    I've moved the data from the feed dictionary to the graph's constant data.
    
    ...

--- tensorflow/reduce_mean_example.py
+++ tensorflow/reduce_mean_example.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 config = tf.ConfigProto(device_count={'GPU': 0})
 
 graph = tf.Graph()
+
 with graph.as_default():
-    x = tf.placeholder(tf.float32, shape=(2, 2))
-    result = tf.reduce_mean(x)
+    x = tf.Variable(tf.constant([[1.0, 1.0], [2.0, 2.0]]))
+    result = tf.reduce_mean(x), tf.reduce_mean(x, 0), tf.reduce_mean(x, 1)
 
 with tf.Session(graph=graph, config=config) as session:
-    print (session.run(result, feed_dict={x: [[1.0, 1.0], [2.0, 2.0]]}))
+    tf.global_variables_initializer().run()
+    print (session.run(result))
