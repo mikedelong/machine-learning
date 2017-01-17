10c09fb44fc5abe99d3d78c34c204c9803016aff
Mike DeLong <mikedelong@outlook.com>
2017-01-17 10:44:20 -0500

    feature: now we're taking the mean of a truncated normal distribution
    Note that the distribution has a seed, so we should get the same result every time we call.
    
    ...

--- tensorflow/reduce_mean_example.py
+++ tensorflow/reduce_mean_example.py
@@ -5,8 +5,10 @@ config = tf.ConfigProto(device_count={'GPU': 0})
 graph = tf.Graph()
 
 with graph.as_default():
-    x = tf.Variable(tf.constant([[1.0, 1.0], [2.0, 2.0]]))
-    result = tf.reduce_mean(x), tf.reduce_mean(x, 0), tf.reduce_mean(x, 1)
+    # x = tf.Variable(tf.constant([[1.0, 1.0], [2.0, 2.0]]))
+    # result = tf.reduce_mean(x), tf.reduce_mean(x, 0), tf.reduce_mean(x, 1)
+    distribution = tf.Variable(tf.truncated_normal([100, 100], mean=0.0, stddev=1.0, seed=0))
+    result = tf.reduce_mean(distribution)
 
 with tf.Session(graph=graph, config=config) as session:
     tf.global_variables_initializer().run()
