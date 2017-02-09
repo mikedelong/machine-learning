25757f93f0ea940835898f7346778632bb7a3a9c
Mike DeLong <mikedelong@outlook.com>
2017-02-09 17:55:35 -0500

    feature: first working version; currently hardcoded to produce 10000 images
    I still have some issues to sort out here; some problematic single-letter images are causing errors, and I don't currently have support for degenerate cases (i.e. 2-, 3-, and 4-letter images).
    
    ...

--- tensorflow/concatenate_images.py
+++ tensorflow/concatenate_images.py
@@ -1,35 +1,55 @@
 import logging
-
+import os
+import random
+import shutil
 from PIL import Image
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s :: %(message)s', level=logging.DEBUG)
 
-import os, random
-
 random_seed = 1
 random.seed(random_seed)
 
-count = 0
-root ='./notMNIST_small/'
-while count < 5:
-    t0 = random.choice(os.listdir(root))
-    t1 = root + t0
-    if os.path.isdir(t1):
-        t2  =  random.choice(os.listdir(t1 + '/'))
-        count += 1
-        logging.debug(t1 + '/' + t2)
+output_folder = './concatenate_output/'
 
-# images = map(Image.open, ['Test1.jpg', 'Test2.jpg', 'Test3.jpg'])
-# widths, heights = zip(*(i.size for i in images))
-#
-# total_width = sum(widths)
-# max_height = max(heights)
-#
-# new_im = Image.new('RGB', (total_width, max_height))
-#
-# x_offset = 0
-# for im in images:
-#     new_im.paste(im, (x_offset, 0))
-#     x_offset += im.size[0]
-#
-# new_im.save('test.jpg')
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+else:
+    shutil.rmtree(output_folder)
+    os.makedirs(output_folder)
+
+count = 0
+root = './notMNIST_small/'
+for file_count in range(0, 10000):
+    try:
+        output_filename = str(file_count)
+        sources = list()
+        while count < 5:
+            t0 = random.choice(os.listdir(root))
+            t1 = root + t0
+            if os.path.isdir(t1):
+                t2 = random.choice(os.listdir(t1 + '/'))
+                output_filename += t0
+                count += 1
+                file_name = t1 + '/' + t2
+                logging.debug(file_name)
+                sources.append(file_name)
+        count = 0
+
+        images = map(Image.open, sources)
+        widths, heights = zip(*(i.size for i in images))
+
+        total_width = sum(widths)
+        max_height = max(heights)
+
+        new_image = Image.new('RGB', (total_width, max_height))
+
+        x_offset = 0
+        for im in images:
+            new_image.paste(im, (x_offset, 0))
+            x_offset += im.size[0]
+
+        output_full_filename = output_folder + output_filename + '.png'
+        new_image.save(output_full_filename)
+        logging.debug('%d %s' % (file_count, output_full_filename))
+    except IOError as io_error:
+        logging.warn(io_error)
