bf957286f498fd34c5619aa1b4b9c440c04f0790
Mike DeLong <mikedelong@outlook.com>
2017-02-11 21:00:58 -0500

    feature: introduced leading blanks
    We want to generate a random 5-digit number and convert it to the appropriate string
    and we want to use one or more leading blanks if the generated number is
    1, 2, 3, or 4 digits.
    
    ...

--- tensorflow/concatenate_images.py
+++ tensorflow/concatenate_images.py
@@ -20,36 +20,35 @@ else:
 count = 0
 root = './notMNIST_small/'
 for file_count in range(0, 10000):
-    try:
-        output_filename = str(file_count)
-        sources = list()
-        while count < 5:
-            t0 = random.choice(os.listdir(root))
-            t1 = root + t0
-            if os.path.isdir(t1):
-                t2 = random.choice(os.listdir(t1 + '/'))
-                output_filename += t0
-                count += 1
-                file_name = t1 + '/' + t2
-                logging.debug(file_name)
-                sources.append(file_name)
-        count = 0
+    output_filename = str(file_count) + '_'
+    current = random.randint(0, 99999)
+    t0 = [c for c in map(int, str(current))]
+    blanks_needed = 5 - len(t0)
+    sources = list()
+    for blanks in range(0, blanks_needed):
+        sources.append('./blank.png')
+    for c0 in t0:
+        t1 = chr(c0 + ord('A'))
+        t2 = root + t1
+        t3 = random.choice(os.listdir(t2 + '/'))
+        output_filename += t1
+        file_name = t2 + '/' + t3
+        logging.debug(file_name)
+        sources.append(file_name)
 
-        images = map(Image.open, sources)
-        widths, heights = zip(*(i.size for i in images))
+    images = map(Image.open, sources)
+    widths, heights = zip(*(i.size for i in images))
 
-        total_width = sum(widths)
-        max_height = max(heights)
+    total_width = sum(widths)
+    max_height = max(heights)
 
-        new_image = Image.new('RGB', (total_width, max_height))
+    new_image = Image.new('RGB', (total_width, max_height))
 
-        x_offset = 0
-        for im in images:
-            new_image.paste(im, (x_offset, 0))
-            x_offset += im.size[0]
+    x_offset = 0
+    for im in images:
+        new_image.paste(im, (x_offset, 0))
+        x_offset += im.size[0]
 
-        output_full_filename = output_folder + '_' + output_filename + '.png'
-        new_image.save(output_full_filename)
-        logging.debug('%d %s' % (file_count, output_full_filename))
-    except IOError as io_error:
-        logging.warn(io_error)
+    output_full_filename = output_folder  + output_filename + '.png'
+    new_image.save(output_full_filename)
+    logging.debug('%d %s' % (file_count, output_full_filename))
